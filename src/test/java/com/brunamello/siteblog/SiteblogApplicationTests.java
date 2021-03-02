51b577f04981eefa23e6b3c318f6a56955c105b7
brunamello <brunacrespomello@gmail.com>
2021-03-02 06:30:05 +0000

    00. First Commit

---
+++ src/test/java/com/brunamello/siteblog/SiteblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunamello.siteblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
