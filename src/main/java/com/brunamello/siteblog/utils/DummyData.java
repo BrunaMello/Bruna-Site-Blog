37af368394a7687bfe081e7b73f93eb9e55a8cd3
brunamello <brunacrespomello@gmail.com>
2021-03-03 01:28:08 +0000

    05. Ajustes Frontend

--- src/main/java/com/brunamello/siteblog/utils/DummyData.java
+++ src/main/java/com/brunamello/siteblog/utils/DummyData.java
@@ -9,31 +9,31 @@ import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class DummyData {
-
-    @Autowired
-    BlogRepository blogRepository;
-
-    @PostConstruct
-    public void savaPosts(){
-
-        List<Post> postList = new ArrayList<>();
-
-        Post post1 = new Post();
-        post1.setAutor("Bruna Mello");
-        post1.setCategoria("Programação");
-        post1.setSub_categoria("Java");
-        post1.setTexto("testtesttesttesttesttesttesttesttesttest");
-        post1.setTitulo("Primeiro Post");
-
-        postList.add(post1);
-
-        for (Post post: postList){
-            Post postSaved = blogRepository.save(post);
-            System.out.println(postSaved.getId());
-        }
-
-
-    }
-}
+//@Component
+//public class DummyData {
+//
+//    @Autowired
+//    BlogRepository blogRepository;
+//
+//    @PostConstruct
+//    public void savaPosts(){
+//
+//        List<Post> postList = new ArrayList<>();
+//
+//        Post post1 = new Post();
+//        post1.setAutor("Bruna Mello");
+//        post1.setCategoria("Programação");
+//        post1.setSub_categoria("Java");
+//        post1.setTexto("testtesttesttesttesttesttesttesttesttest");
+//        post1.setTitulo("Primeiro Post");
+//
+//        postList.add(post1);
+//
+//        for (Post post: postList){
+//            Post postSaved = blogRepository.save(post);
+//            System.out.println(postSaved.getId());
+//        }
+//
+//
+//    }
+//}
